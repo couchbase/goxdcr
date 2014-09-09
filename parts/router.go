2d6eeac6cc09fa4205ada13e959f4950be90b49b
Yu Sui <yu@Yus-MacBook-Pro.local>
2014-09-09 12:49:23 -0700

    add router

---
+++ parts/router.go
@@ -0,0 +1,153 @@
+package parts
+
+import (
+	"errors"
+	"math"
+	"encoding/binary"
+	mc "github.com/couchbase/gomemcached"
+	mcc "github.com/couchbase/gomemcached/client"
+	common "github.com/Xiaomei-Zhang/couchbase_goxdcr/common"
+	connector "github.com/Xiaomei-Zhang/couchbase_goxdcr/connector"
+	log "github.com/Xiaomei-Zhang/couchbase_goxdcr/util"
+)
+
+var ErrorInvalidDataForRouter = errors.New("Input data to Router is invalid.")
+var ErrorNoDownStreamNodesForRouter = errors.New("No downstream nodes have been defined for the Router")
+
+var logger_router *log.CommonLogger = log.NewLogger("Router", log.LogLevelInfo)
+
+// XDCR Router does two things:
+// 1. converts UprEvent to MCRequest
+// 2. routes MCRequest to downstream parts
+type Router struct {
+	*connector.Router
+	numOfVbuckets uint16
+	vbMap map[uint16]string    // pvbno -> partId. This defines which vbnos would be routed to which part
+}
+
+func NewRouter(downStreamParts map[string]common.Part, numOfVbuckets uint16) (*Router, error){
+	router := &Router{
+				numOfVbuckets: numOfVbuckets,
+				vbMap: make(map[uint16]string),
+			   }
+			   
+	var routingFunc connector.Routing_Callback_Func = router.route
+	router.Router = connector.NewRouter(downStreamParts, &routingFunc)
+	
+	err := router.buildVbMap()
+	if err == nil {
+		logger_router.Infof("Router created with %d downstream parts \n", len(downStreamParts))
+		return router, nil
+	} else {
+		return nil, err
+	}
+}
+
+// build vbMap for load balancing
+// currently the default load balancing strategy is to evenly distribute vbuckets among downstream parts
+func (router *Router) buildVbMap() error {
+	router.vbMap = make(map[uint16]string)
+	
+	downStreamParts := router.DownStreams()
+	numOfNodes := len(downStreamParts)
+	if numOfNodes == 0 {
+		return ErrorNoDownStreamNodesForRouter
+	}
+	
+	numOfVbPerNode := uint16(math.Ceil(float64(router.numOfVbuckets)/float64(numOfNodes)))
+	
+	var indexOfNode uint16
+	for partId := range downStreamParts {
+	    var j uint16
+		for j = 0; j < numOfVbPerNode; j++ {
+			vbno := indexOfNode * numOfVbPerNode + j
+			if vbno < router.numOfVbuckets {
+				router.vbMap[vbno] = partId
+			} else {
+				// no more vbs to process 
+				break
+			}
+		}
+		indexOfNode ++
+	}
+	
+	logger_router.Debugf("Router initialized. Distributed a total of %d vbuckets to %d parts, each having %d vbuckets.\n", router.numOfVbuckets, numOfNodes, numOfVbPerNode)
+	logger_router.Debugf("Router vbMap: %v", router.vbMap) 
+	return nil
+	
+}
+
+func ComposeMCRequest(event *mcc.UprEvent) *mc.MCRequest {
+	req := &mc.MCRequest{Cas: event.Cas,
+		Opaque:  0,
+		VBucket: event.VBucket,
+		Key:     event.Key,
+		Body:    event.Value,
+		Extras:	 make ([]byte, 224)}
+	//opCode
+	switch event.Opcode {
+	case mcc.UprStreamRequest:
+		req.Opcode = mc.UPR_STREAMREQ
+	case mcc.UprMutation:
+		req.Opcode = mc.UPR_MUTATION
+	case mcc.UprDeletion:
+		req.Opcode = mc.UPR_DELETION
+	case mcc.UprExpiration:
+		req.Opcode = mc.UPR_EXPIRATION
+	case mcc.UprCloseStream:
+		req.Opcode = mc.UPR_CLOSESTREAM
+	case mcc.UprSnapshot:
+		req.Opcode = mc.UPR_SNAPSHOT
+	case mcc.UprFlush:
+		req.Opcode = mc.UPR_FLUSH
+	}
+
+	//extra
+	if event.Opcode == mcc.UprMutation || event.Opcode == mcc.UprDeletion ||
+		event.Opcode == mcc.UprExpiration {
+		binary.BigEndian.PutUint64(req.Extras, event.Seqno)
+		binary.BigEndian.PutUint32(req.Extras, event.Flags)
+		binary.BigEndian.PutUint32(req.Extras, event.Expiry)
+	} else if event.Opcode == mcc.UprSnapshot {
+		binary.BigEndian.PutUint64(req.Extras, event.Seqno)
+		binary.BigEndian.PutUint64(req.Extras, event.SnapstartSeq)
+		binary.BigEndian.PutUint64(req.Extras, event.SnapendSeq)
+		binary.BigEndian.PutUint32(req.Extras, event.SnapshotType)
+	}
+
+	return req
+}
+
+// Implementation of the routing algorithm
+// Currently doing static dispatching based on vbucket number. 
+func (router *Router) route(data interface{}) (map[string]interface{}, error) {
+	// only *mc.UprEvent type data is accepted
+	uprEvent, ok := data.(*mcc.UprEvent)
+    if !ok {
+    	return nil, ErrorInvalidDataForRouter
+    }
+    
+    // use vbMap to determine which downstream part to route the request
+    partId := router.vbMap[uprEvent.VBucket]
+	logger_router.Debugf("Data with vbno %d is routed to downstream part %s", uprEvent.VBucket, partId)
+
+	result := make(map[string]interface{})
+	result[partId] = ComposeMCRequest(uprEvent)
+	
+	return result, nil
+}
+
+func (router *Router) AddDownStream(partId string, part common.Part) error {
+	if router.DownStreams()[partId] == nil {
+		err := router.Router.AddDownStream(partId, part)
+		if err == nil {
+			// re-build vbmap
+			return router.buildVbMap()
+		} else {
+			return err
+		}
+	} else {
+		return router.Router.AddDownStream(partId, part)
+	}
+	
+}
---
+++ tests/router/router_run.go
@@ -0,0 +1,190 @@
+// Tool receives raw events from go-couchbase UPR client.
+package main
+
+import (
+	"flag"
+	"fmt"
+	parts "github.com/Xiaomei-Zhang/couchbase_goxdcr_impl/parts"
+	part "github.com/Xiaomei-Zhang/couchbase_goxdcr/part"
+	"github.com/couchbase/indexing/secondary/common"
+	"github.com/couchbaselabs/go-couchbase"
+	"log"
+	"os"
+	"sync"
+	"strconv"
+	pc "github.com/Xiaomei-Zhang/couchbase_goxdcr/common"
+	mc "github.com/couchbase/gomemcached"
+)
+
+var options struct {
+	source_bucket string // source bucket
+	target_bucket string //target bucket
+	connectStr    string //connect string
+	username      string //username
+	password      string //password
+	maxVbno       int    // maximum number of vbuckets
+}
+
+var done = make(chan bool, 16)
+var rch = make(chan []interface{}, 10000)
+var uprFeed *couchbase.UprFeed = nil
+var router *parts.Router = nil
+
+const (
+    // total number of parts to route data to
+	NUM_PARTS = 3
+	// total number of data points to be routed
+	NUM_DATA = 100 
+	// prefix for part id
+	PART_ID_PREFIX = "part"
+)
+
+
+func argParse() {
+
+	flag.StringVar(&options.source_bucket, "source_bucket", "default",
+		"bucket to replicate from")
+	flag.IntVar(&options.maxVbno, "maxvb", 8,
+		"maximum number of vbuckets")
+	flag.StringVar(&options.target_bucket, "target_bucket", "target",
+		"bucket to replicate to")
+
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 1 {
+		usage()
+		os.Exit(1)
+	}
+	options.connectStr = args[0]
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage : %s [OPTIONS] <cluster-addr> \n", os.Args[0])
+	flag.PrintDefaults()
+}
+
+func main() {
+	fmt.Println("Start Testing Router...")
+	argParse()
+	fmt.Printf("connectStr=%s\n", options.connectStr)
+	fmt.Println("Done with parsing the arguments")
+	startRouter()
+	fmt.Println("Router is started")
+	waitGrp := &sync.WaitGroup{}
+	waitGrp.Add(1)
+	go startUpr(options.connectStr, options.source_bucket, waitGrp)
+	waitGrp.Wait()
+}
+
+func startUpr(cluster, bucketn string, waitGrp *sync.WaitGroup) {
+	b, err := common.ConnectBucket(cluster, "default", bucketn)
+	mf(err, "bucket")
+
+	uprFeed, err = b.StartUprFeed("rawupr", uint32(0))
+	mf(err, "- upr")
+
+	flogs := failoverLogs(b)
+	fmt.Print("Got failover log successfully")
+
+	// list of vbuckets
+	vbnos := make([]uint16, 0, options.maxVbno)
+	for i := 0; i < options.maxVbno; i++ {
+		vbnos = append(vbnos, uint16(i))
+	}
+
+	startStream(uprFeed, flogs)
+	fmt.Print("Upr stream is started")
+
+	count := 0
+	for {
+		e, ok := <-uprFeed.C
+		if ok == false {
+			fmt.Println("Closing for bucket", b.Name)
+		}
+
+		// let router process the stream
+		count++
+		fmt.Println("Number of upr event received so far is %d", count)
+		err := router.Forward(e)
+		mf(err, " - route")
+		
+
+		if count >= NUM_DATA {
+			break
+		}
+
+	}
+
+	//close the upr stream
+	uprFeed.Close()
+	waitGrp.Done()
+}
+
+func startStream(uprFeed *couchbase.UprFeed, flogs couchbase.FailoverLog) {
+	start, end := uint64(0), uint64(0xFFFFFFFFFFFFFFFF)
+	snapStart, snapEnd := uint64(0), uint64(0)
+	for vbno, flog := range flogs {
+		x := flog[len(flog)-1] // map[uint16][][2]uint64
+		flags, vbuuid := uint32(0), x[0]
+		err := uprFeed.UprRequestStream(
+			vbno, flags, vbuuid, start, end, snapStart, snapEnd)
+		mf(err, fmt.Sprintf("stream-req for %v failed", vbno))
+	}
+}
+
+func failoverLogs(b *couchbase.Bucket) couchbase.FailoverLog {
+	// list of vbuckets
+	vbnos := make([]uint16, 0, options.maxVbno)
+	for i := 0; i < options.maxVbno; i++ {
+		vbnos = append(vbnos, uint16(i))
+	}
+
+	flogs, err := b.GetFailoverLogs(vbnos)
+	mf(err, "- upr failoverlogs")
+	return flogs
+}
+
+func mf(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%v: %v", msg, err)
+	}
+}
+
+func startRouter() {
+	
+	partMaps := make(map[string]pc.Part)
+	for i := 0; i < NUM_PARTS; i++ {
+		partId := PART_ID_PREFIX + strconv.FormatInt(int64(i), 10)
+		partMaps[partId] = NewFakePart(partId)
+	}
+	
+	router, _ = parts.NewRouter(partMaps, uint16(options.maxVbno))
+}
+
+type FakePart struct {
+	part.AbstractPart
+}
+
+func NewFakePart(id string) *FakePart {
+	fp := new(FakePart)
+	fp.AbstractPart = part.NewAbstractPart(id)
+	return fp
+}
+
+func (fp *FakePart) Start (settings map[string]interface{} ) error {
+	return nil
+}
+
+func (fp *FakePart) Stop () error {
+	return nil
+}
+
+func (fp *FakePart) Receive (data interface {}) error {
+	fmt.Println("Part %v received data with vbno %v", fp.Id(), data.(*mc.MCRequest).VBucket)
+
+	return nil
+}
+
+func (fp *FakePart) IsStarted () bool {
+	return false
+}
