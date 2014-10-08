9006e81e0ae297f4a18e709aec49825707f3b73f
yu <yu@couchbase.com>
2014-10-08 12:16:58 -0700

    accomodate upr api changes

--- tests/router/router_run.go
+++ tests/router/router_run.go
@@ -147,7 +147,7 @@ func startStream(uprFeed *couchbase.UprFeed, flogs couchbase.FailoverLog) {
 		x := flog[len(flog)-1] // map[uint16][][2]uint64
 		flags, vbuuid := uint32(0), x[0]
 		err := uprFeed.UprRequestStream(
-			vbno, flags, vbuuid, start, end, snapStart, snapEnd)
+			vbno, uint32(vbno), flags, vbuuid, start, end, snapStart, snapEnd)
 		mf(err, fmt.Sprintf("stream-req for %v failed", vbno))
 	}
 }
