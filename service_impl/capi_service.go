ef9344f4ee10bb4ca2c98c9cfc3accd48ca5a656
yu <yu@couchbase.com>
2015-05-08 18:26:52 -0700

    MB-14863 fix issue with massVbopaqueCheck
    Change-Id: I9d807474d2a2880dc0aad7fed608ee8f5ed69901
    Reviewed-on: http://review.couchbase.org/50803
    Reviewed-by: Xiaomei Zhang <xiaomei@couchbase.com>
    Tested-by: Yu Sui <ysui68@gmail.com>

--- service_impl/capi_service.go
+++ service_impl/capi_service.go
@@ -63,7 +63,7 @@ func NewCAPIService(cluster_info_service *ClusterInfoSvc, logger_ctx *log.Logger
 func (capi_svc *CAPIService) PreReplicate(remoteBucket *service_def.RemoteBucketInfo,
 	knownRemoteVBStatus *service_def.RemoteVBReplicationStatus, xdcrCheckpoingCapbility bool) (bMatch bool, current_remoteVBOpaque metadata.TargetVBOpaque, err error) {
 	capi_svc.logger.Debug("Calling _pre_replicate")
-	api_base, err := capi_svc.composeAPIRequestBase(remoteBucket, knownRemoteVBStatus.VBNo)
+	api_base, err := capi_svc.composeAPIRequestBaseForVb(remoteBucket, knownRemoteVBStatus.VBNo)
 	if err != nil {
 		return
 	}
@@ -73,7 +73,7 @@ func (capi_svc *CAPIService) PreReplicate(remoteBucket *service_def.RemoteBucket
 	}
 
 	capi_svc.logger.Debugf("request to _pre_replicate = %v\n", api_base)
-	http_client := remoteBucket.RestAddrHttpClientMap [api_base.url]
+	http_client := remoteBucket.RestAddrHttpClientMap[api_base.url]
 	status_code, respMap, _, err := capi_svc.send_post(PRE_REPLICATE_CMD, api_base, http_client, HTTP_RETRIES)
 	capi_svc.logger.Debugf("response from _pre_replicate is status_code=%v respMap=%v for %v\n", status_code, respMap, knownRemoteVBStatus)
 	if err != nil {
@@ -99,13 +99,13 @@ func (capi_svc *CAPIService) PreReplicate(remoteBucket *service_def.RemoteBucket
 //			  err
 func (capi_svc *CAPIService) CommitForCheckpoint(remoteBucket *service_def.RemoteBucketInfo, remoteVBOpaque metadata.TargetVBOpaque, vbno uint16) (remote_seqno uint64, vbOpaque metadata.TargetVBOpaque, err error) {
 	capi_svc.logger.Debug("Calling _commit_for_checkpoint")
-	api_base, err := capi_svc.composeAPIRequestBase(remoteBucket, vbno)
+	api_base, err := capi_svc.composeAPIRequestBaseForVb(remoteBucket, vbno)
 	if err != nil {
 		return
 	}
 	api_base.body["vb"] = vbno
 	api_base.body["vbopaque"] = remoteVBOpaque.Value()
-	http_client := remoteBucket.RestAddrHttpClientMap [api_base.url]
+	http_client := remoteBucket.RestAddrHttpClientMap[api_base.url]
 	status_code, respMap, _, err := capi_svc.send_post(COMMIT_FOR_CKPT_CMD, api_base, http_client, HTTP_RETRIES)
 
 	if err == nil && status_code == 400 {
@@ -160,20 +160,45 @@ func (capi_svc *CAPIService) CommitForCheckpoint(remoteBucket *service_def.Remot
 func (capi_svc *CAPIService) MassValidateVBUUIDs(remoteBucket *service_def.RemoteBucketInfo, remoteVBUUIDs map[uint16]metadata.TargetVBOpaque) (matching []uint16, mismatching []uint16, missing []uint16, err error) {
 	capi_svc.logger.Info("Calling _mass_vbopaque_check")
 
-	//use the vbucket 0's capi api url
-	api_base, err := capi_svc.composeAPIRequestBase(remoteBucket, 0)
+	matching = make([]uint16, 0)
+	mismatching = make([]uint16, 0)
+	missing = make([]uint16, 0)
+	for server_addr, vblist := range remoteBucket.VBServerMap {
+		vbopaques := [][]interface{}{}
+		for _, vbno := range vblist {
+			if remotevbuuid, ok := remoteVBUUIDs[vbno]; ok && remotevbuuid != nil {
+				vbopaques = append(vbopaques, []interface{}{vbno, remotevbuuid.Value()})
+			}
+		}
+
+		if len(vbopaques) == 0 {
+			continue
+		}
+
+		matching_per_server, mismatching_per_server, missing_per_server, err1 := capi_svc.massValidateVBUUIDsForServer(remoteBucket, vbopaques, server_addr, vblist)
+		if err1 != nil {
+			err = err1
+			return
+		}
+		matching = append(matching, matching_per_server...)
+		mismatching = append(mismatching, mismatching_per_server...)
+		missing = append(missing, missing_per_server...)
+	}
+
+	return
+}
+
+func (capi_svc *CAPIService) massValidateVBUUIDsForServer(remoteBucket *service_def.RemoteBucketInfo, vbopaques [][]interface{},
+	server_addr string, vblist []uint16) (matching []uint16, mismatching []uint16, missing []uint16, err error) {
+	capi_svc.logger.Infof("Calling _mass_vbopaque_check for server %v\n", server_addr)
+
+	api_base, err := capi_svc.composeAPIRequestBaseForServer(remoteBucket, server_addr)
 	if err != nil {
 		return
 	}
 
-	vbopaques := [][]interface{}{}
-	for vb, remotevbuuid := range remoteVBUUIDs {
-		if remotevbuuid != nil {
-			vbopaques = append(vbopaques, []interface{}{vb, remotevbuuid.Value()})
-		}
-	}
 	api_base.body["vbopaques"] = vbopaques
-	http_client := remoteBucket.RestAddrHttpClientMap [api_base.url]
+	http_client := remoteBucket.RestAddrHttpClientMap[api_base.url]
 	status_code, respMap, _, err := capi_svc.send_post(MASS_VBOPAQUE_CHECK_CMD, api_base, http_client, HTTP_RETRIES)
 
 	capi_svc.logger.Debugf("vbopaques=%v\n", vbopaques)
@@ -225,26 +250,38 @@ func (capi_svc *CAPIService) parseMassValidateSeqNosResp(url string, resp_status
 	return
 }
 
-func (capi_svc *CAPIService) composeAPIRequestBase(remoteBucket *service_def.RemoteBucketInfo, vbno uint16) (*apiRequest, error) {
+func (capi_svc *CAPIService) composeAPIRequestBaseForVb(remoteBucket *service_def.RemoteBucketInfo, vbno uint16) (*apiRequest, error) {
+	rest_addr, err := capi_svc.lookUpConnectionStrForVb(remoteBucket, vbno)
+	if err != nil {
+		return nil, err
+	}
+
+	return capi_svc.composeAPIRequestBase(remoteBucket, rest_addr)
+}
+
+func (capi_svc *CAPIService) composeAPIRequestBaseForServer(remoteBucket *service_def.RemoteBucketInfo, server_addr string) (*apiRequest, error) {
+	rest_addr, err := capi_svc.lookUpConnectionStrForServer(remoteBucket, server_addr)
+	if err != nil {
+		return nil, err
+	}
+
+	return capi_svc.composeAPIRequestBase(remoteBucket, rest_addr)
+}
+
+func (capi_svc *CAPIService) composeAPIRequestBase(remoteBucket *service_def.RemoteBucketInfo, rest_addr string) (*apiRequest, error) {
 	if remoteBucket.RemoteClusterRef == nil || remoteBucket.UUID == "" {
 		return nil, errors.New("Remote Bucket information is not fully populated")
 	}
 
-	connectionStr, err := capi_svc.lookUpConnectionStr(remoteBucket, vbno)
-	if err != nil {
-		return nil, err
-	}
 	username, password, err := remoteBucket.RemoteClusterRef.MyCredentials()
 	if err != nil {
 		return nil, err
 	}
 
-	couchApiBaseUrl := connectionStr
-
 	//	    BodyBase = [{<<"bucket">>, Bucket},
 	//                {<<"bucketUUID">>, BucketUUID}],
 	api_base := &apiRequest{}
-	api_base.url = couchApiBaseUrl
+	api_base.url = rest_addr
 	api_base.username = username
 	api_base.password = password
 	api_base.body = make(map[string]interface{})
@@ -378,7 +415,7 @@ func invalidVBOpaqueError(resp_status_code int,
 	return fmt.Errorf("invalid vbopaque in _pre_replicate response. status_code=%v respMap=%v vbno=%v\n", resp_status_code, respMap, vbno)
 }
 
-func (capi_svc *CAPIService) lookUpConnectionStr(remoteBucket *service_def.RemoteBucketInfo, vbno uint16) (string, error) {
+func (capi_svc *CAPIService) lookUpConnectionStrForVb(remoteBucket *service_def.RemoteBucketInfo, vbno uint16) (string, error) {
 	if remoteBucket == nil {
 		return "", errors.New("Not a valid remote bucket, VBServerMap is not populated correctly")
 	}
@@ -394,14 +431,18 @@ func (capi_svc *CAPIService) lookUpConnectionStr(remoteBucket *service_def.Remot
 	}
 
 Done:
-	if foundServerAddr != "" {
-		connectionStr, ok := remoteBucket.MemcachedAddrRestAddrMap[foundServerAddr]
+	return capi_svc.lookUpConnectionStrForServer(remoteBucket, foundServerAddr)
+}
+
+func (capi_svc *CAPIService) lookUpConnectionStrForServer(remoteBucket *service_def.RemoteBucketInfo, server_addr string) (string, error) {
+	if server_addr != "" {
+		connectionStr, ok := remoteBucket.MemcachedAddrRestAddrMap[server_addr]
 		if !ok {
-			return "", errors.New(fmt.Sprintf("failed to find server for vb=%v", vbno))
+			return "", errors.New(fmt.Sprintf("failed to find rest addr for _mass_vbopaque_check for server=%v", server_addr))
 		}
 		return connectionStr, nil
 	} else {
-		return "", errors.New(fmt.Sprintf("failed to find server for vb=%v", vbno))
+		return "", errors.New(fmt.Sprintf("server addr is nil"))
 	}
 
 }
