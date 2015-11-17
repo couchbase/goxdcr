8fd4f61adffefd84efab26c2900ace7b1b1590b3
yu <yu@couchbase.com>
2015-11-17 12:46:18 -0800

    MB-16853 do not close connections returned to pool
    Change-Id: I9bbf72047e38928b60985cae9e19b2172fd961a0
    Reviewed-on: http://review.couchbase.org/57125
    Reviewed-by: Ritesh Motlani <ritesh.motlani@couchbase.com>
    Reviewed-by: John Liang <johliang00@yahoo.com>
    Tested-by: Yu Sui <ysui68@gmail.com>

--- parts/xmem_nozzle.go
+++ parts/xmem_nozzle.go
@@ -641,6 +641,7 @@ func (client *xmemClient) close() {
 		pool := base.ConnPoolMgr().GetPool(client.poolName)
 		if pool != nil {
 			pool.Release(client.memClient)
+			return
 		}
 	}
 	client.memClient.Close()
