b73418391280315f96973d5d25b840f7e93c7c42
yu <yu@couchbase.com>
2017-11-07 10:44:10 -0800

    MB-25459 do not write default replication settings to metakv
    Change-Id: Id03f122c7649acc6212e7650d820e8fa945ed9d4
    Reviewed-on: http://review.couchbase.org/85198
    Reviewed-by: Neil Huang <neil.huang@couchbase.com>
    Reviewed-by: John Liang <johliang00@yahoo.com>
    Reviewed-by: Yu Sui <ysui68@gmail.com>
    Tested-by: Yu Sui <ysui68@gmail.com>

--- metadata_svc/replication_settings_service.go
+++ metadata_svc/replication_settings_service.go
@@ -39,15 +39,6 @@ func (repl_settings_svc *ReplicationSettingsSvc) GetDefaultReplicationSettings()
 	if err == service_def.MetadataNotFoundErr {
 		// initialize default settings if it does not exist
 		defaultSettings = *metadata.DefaultSettings()
-		repl_settings_svc.SetDefaultReplicationSettings(&defaultSettings)
-
-		// reload default settings to get its revision field set correctly
-		_, rev, err := repl_settings_svc.metadata_svc.Get(DefaultReplicationSettingsKey)
-		if err != nil {
-			return nil, err
-		}
-		// set rev number
-		defaultSettings.Revision = rev
 	} else {
 		err = json.Unmarshal(bytes, &defaultSettings)
 		if err != nil {
