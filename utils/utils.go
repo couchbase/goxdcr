e85ac07cfe5ce3eb13a2c12bf82e8490c7ce10c0
yu <yu@couchbase.com>
2018-10-01 17:55:23 -0700

    MB-31479 fix panic in exponentialExecutor
    Change-Id: If997dafa96f51140d11e0e5c37df7b9251233f49
    Reviewed-on: http://review.couchbase.org/100166
    Reviewed-by: Neil Huang <neil.huang@couchbase.com>
    Reviewed-by: Yu Sui <ysui68@gmail.com>
    Tested-by: Yu Sui <ysui68@gmail.com>

--- utils/utils.go
+++ utils/utils.go
@@ -2399,8 +2399,9 @@ func (u *Utilities) ExponentialBackoffExecutorWithFinishSignal(name string, init
 	for i := 0; i <= maxRetries; i++ {
 		select {
 		case <-finCh:
-			u.logger_utils.Warnf("ExponentialBackoffExecutorWithFinishSignal for %v aborting because of finch closure\n", name)
-			break
+			err = fmt.Errorf("ExponentialBackoffExecutorWithFinishSignal for %v aborting because of finch closure\n", name)
+			u.logger_utils.Warnf(err.Error())
+			return nil, err
 		default:
 			result, err = op(param)
 			if err == nil {
