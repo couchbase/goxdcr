1000cc09f9cfb10d770fc9d40ca7b05a5b402314
Neil Huang <neil.huang@couchbase.com>
2019-06-21 20:46:08 -0700

    MB-34568 - compression test suite and library updates to enable test suite to work
    Change-Id: I1a0fd9ea069651db51acab3fb8535b4ac5387eac
    Reviewed-on: http://review.couchbase.org/111063
    Reviewed-by: Neil Huang <neil.huang@couchbase.com>
    Tested-by: Neil Huang <neil.huang@couchbase.com>

--- tools/testScripts/compressionTest.sh
+++ tools/testScripts/compressionTest.sh
@@ -40,14 +40,9 @@ cluster2BucketsArr=("B2" "B3")
 CLUSTER_NAME_BUCKET_MAP=(["C1"]=${cluster1BucketsArr[@]}  ["C2"]=${cluster2BucketsArr[@]})
 
 # Bucket properties
-declare -A Bucket0Properties=(["ramQuotaMB"]=100 ["compressionMode"]="off")
-declare -A Bucket1Properties=(["ramQuotaMB"]=100 ["compressionMode"]="active")
-declare -A Bucket2Properties=(["ramQuotaMB"]=100 ["compressionMode"]="off")
-declare -A Bucket3Properties=(["ramQuotaMB"]=100 ["compressionMode"]="off")
-insertPropertyIntoBucketNamePropertyMap "B0" Bucket0Properties
-insertPropertyIntoBucketNamePropertyMap "B1" Bucket1Properties
-insertPropertyIntoBucketNamePropertyMap "B2" Bucket2Properties
-insertPropertyIntoBucketNamePropertyMap "B3" Bucket3Properties
+declare -A BucketNoCompression=(["ramQuotaMB"]=100 ["compressionMode"]="off")
+declare -A BucketActiveCompression=(["ramQuotaMB"]=100 ["compressionMode"]="active")
+declare -A BucketPassiveCompression=(["ramQuotaMB"]=100 ["compressionMode"]="passive")
 
 declare -A NoCompressionReplProperties=(["replicationType"]="continuous" ["checkpointInterval"]=60 ["statsInterval"]=500 ["compressionType"]="None")
 declare -A CompressionReplProperties=(["replicationType"]="continuous" ["checkpointInterval"]=60 ["statsInterval"]=500 ["compressionType"]="Auto")
@@ -65,6 +60,8 @@ declare -A CompressionReplProperties=(["replicationType"]="continuous" ["checkpo
 
 
 function runDataLoad {
+	# sleep 3 seconds before running
+	sleep 3
 	# Run CBWorkloadgen in parallel
 	runCbWorkloadGenBucket "C1" "B0" &
 	runCbWorkloadGenBucket "C1" "B1" &
@@ -73,40 +70,115 @@ function runDataLoad {
 	waitForBgJobs
 }
 
+EXPECTED_CNT=20000
 function checkItemCnt {
 	local cluster=$1
 	local bucket=$2
-	
+	local expectedCnt=$3
+
 	itemCount=`getBucketItemCount "$cluster" "$bucket"`
-	echo "Item count for cluster $cluster bucket $bucket: $itemCount"
+	if (( $itemCount == $expectedCnt ));then
+		echo "Item count for cluster $cluster bucket $bucket: $itemCount"
+	else
+		echo "ERROR: Cluster $cluster bucket $bucket only has $itemCount items"
+		exit 1
+	fi
 }
 
 #MAIN
+sleepTime=10
 testForClusterRun
 if (( $? != 0 ));then
 	exit $?
 fi
+sleep 1
 
-setupTopologies
-if (( $? != 0 ));then
-	exit $?
-fi
-# Wait for vbuckets and all the other things to propagate before XDCR provisioning
-sleep 1
-createRemoteClusterReference "C1" "C2"
-createRemoteClusterReference "C2" "C1"
-sleep 1
-runDataLoad
-sleep 1
-createBucketReplication "C1" "B0" "C2" "B2" NoCompressionReplProperties
-createBucketReplication "C1" "B1" "C2" "B3" NoCompressionReplProperties
-createBucketReplication "C2" "B2" "C1" "B0" NoCompressionReplProperties
-createBucketReplication "C2" "B3" "C1" "B1" NoCompressionReplProperties
+function runTestCase {
+	local testCaseName=$1
+	local sourceBucketPolicy=$2
+	local targetBucketPolicy=$3
+	local replPolicy=$4
 
-sleep 1
-checkItemCnt "C1" "B1"
-checkItemCnt "C1" "B2"
-checkItemCnt "C2" "B3"
-checkItemCnt "C2" "B4"
+	echo "TEST CASE $testCaseName SourceBucket: $sourceBucketPolicy TargetBucket: $targetBucketPolicy CompressionMode: $replPolicy"
+	cleanupBucketNamePropertyMap
+	if [[ "$sourceBucketPolicy" == "Active" ]];then
+		insertPropertyIntoBucketNamePropertyMap "B0" BucketActiveCompression
+		insertPropertyIntoBucketNamePropertyMap "B1" BucketActiveCompression
+	elif [[ "$sourceBucketPolicy" == "Passive" ]];then
+		insertPropertyIntoBucketNamePropertyMap "B0" BucketPassiveCompression
+		insertPropertyIntoBucketNamePropertyMap "B1" BucketPassiveCompression
+	elif [[ "$sourceBucketPolicy" == "None" ]];then
+		insertPropertyIntoBucketNamePropertyMap "B0" BucketNoCompression
+		insertPropertyIntoBucketNamePropertyMap "B1" BucketNoCompression
+	fi
 
-#exportProvisionedConfig
+	if [[ "$targetBucketPolicy" == "Active" ]];then
+		insertPropertyIntoBucketNamePropertyMap "B2" BucketActiveCompression
+		insertPropertyIntoBucketNamePropertyMap "B3" BucketActiveCompression
+	elif [[ "$targetBucketPolicy" == "Passive" ]];then
+		insertPropertyIntoBucketNamePropertyMap "B2" BucketPassiveCompression
+		insertPropertyIntoBucketNamePropertyMap "B3" BucketPassiveCompression
+	elif [[ "$targetBucketPolicy" == "None" ]];then
+		insertPropertyIntoBucketNamePropertyMap "B2" BucketNoCompression
+		insertPropertyIntoBucketNamePropertyMap "B3" BucketNoCompression
+	fi
+
+	setupTopologies
+	if [[ "$testCaseName" == "1a" ]];then
+		# create rC reference once
+		createRemoteClusterReference "C1" "C2"
+		createRemoteClusterReference "C2" "C1"
+		if (( $? != 0 ));then
+			exit $?
+		fi
+	fi
+
+	# Wait for vbuckets and all the other things to propagate before XDCR provisioning
+	sleep 1
+	runDataLoad
+	sleep 1
+
+	if [[ "$replPolicy" == "Auto" ]];then
+		createBucketReplication "C1" "B0" "C2" "B2" CompressionReplProperties
+		createBucketReplication "C1" "B1" "C2" "B3" CompressionReplProperties
+		createBucketReplication "C2" "B2" "C1" "B0" CompressionReplProperties
+		createBucketReplication "C2" "B3" "C1" "B1" CompressionReplProperties
+	elif [[ "$replPolicy" == "None" ]];then
+		createBucketReplication "C1" "B0" "C2" "B2" NoCompressionReplProperties
+		createBucketReplication "C1" "B1" "C2" "B3" NoCompressionReplProperties
+		createBucketReplication "C2" "B2" "C1" "B0" NoCompressionReplProperties
+		createBucketReplication "C2" "B3" "C1" "B1" NoCompressionReplProperties
+	fi
+
+	echo "Waiting $sleepTime seconds for I/O to quiesce"
+	sleep $sleepTime
+	checkItemCnt "C1" "B0" $EXPECTED_CNT
+	checkItemCnt "C1" "B1" $EXPECTED_CNT
+	checkItemCnt "C2" "B2" $EXPECTED_CNT
+	checkItemCnt "C2" "B3" $EXPECTED_CNT
+
+	# Cleaning up buckets should remove all existing replications
+	echo "TEST CASE $testCaseName passed"
+	cleanupBucketReplications
+	cleanupBuckets
+	sleep $sleepTime
+}
+
+runTestCase "1a" "Active" "Active" "Auto"
+runTestCase "1b" "Active" "Active" "None"
+runTestCase "2a" "Active" "Passive" "Auto"
+runTestCase "2b" "Active" "Passive" "None"
+runTestCase "3a" "Active" "None" "Auto"
+runTestCase "3b" "Active" "None" "Auto"
+runTestCase "4a" "Passive" "Active" "Auto"
+runTestCase "4b" "Passive" "Active" "None"
+runTestCase "5a" "Passive" "Passive" "Auto"
+runTestCase "5b" "Passive" "Passive" "None"
+runTestCase "6a" "Passive" "None" "Auto"
+runTestCase "6b" "Passive" "None" "None"
+runTestCase "7a" "None" "Active" "Auto"
+runTestCase "7b" "None" "Active" "None"
+runTestCase "8a" "None" "Passive" "Auto"
+runTestCase "8b" "None" "Passive" "None"
+runTestCase "9a" "None" "None" "Auto"
+runTestCase "9b" "None" "None" "None"
