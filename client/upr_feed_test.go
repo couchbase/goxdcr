93520e9f1c2aaaef5a448bcf69374c9050973491
Neil Huang <neil.huang@couchbase.com>
2019-09-04 17:55:22 -0700

    MB-35806 - gomemcached to handle collections system events
    Change-Id: I8035da5fcdfe44062164a12ce96bfab4b9b74091

--- client/upr_feed_test.go
+++ client/upr_feed_test.go
@@ -3,9 +3,11 @@ package memcached
 import (
 	"bytes"
 	"encoding/binary"
+	"encoding/json"
 	"fmt"
 	"github.com/couchbase/gomemcached"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"testing"
 )
 
@@ -111,4 +113,181 @@ func TestNegotiatorMultiSession(t *testing.T) {
 	assert.NotNil(err)
 
 	fmt.Println("============== Test case end: TestNegotiatorMultiSession =================")
+}
+
+func retrieveMcRequest(fileName string) *gomemcached.MCRequest {
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	request := &gomemcached.MCRequest{}
+	err = json.Unmarshal(data, request)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return request
+}
+
+func TestCreateScopeEvent(t *testing.T) {
+	assert := assert.New(t)
+	fmt.Println("============== Test case TestCreateScopeEvent =================")
+
+	scopeCreationFile := "./unitTestData/scopeCreation.json"
+	mcReq := retrieveMcRequest(scopeCreationFile)
+
+	assert.NotNil(mcReq)
+
+	assert.Equal(uint16(511), vbOpaque(mcReq.Opaque))
+
+	tempStream := &UprStream{Vbucket: mcReq.VBucket}
+	tempBytes := 1024
+
+	event := makeUprEvent(*mcReq, tempStream, tempBytes)
+	assert.Equal(gomemcached.DCP_SYSTEM_EVENT, event.Opcode)
+	assert.True(event.IsCollectionType())
+	// The key should be the name of the scope being created
+	assert.Equal([]byte("S1"), event.Key)
+	assert.Equal(event.SystemEvent, ScopeCreate)
+
+	creationEvent := ScopeCreateEvent(event)
+	checkScopeName, err := creationEvent.GetName()
+	assert.Nil(err)
+	assert.Equal("S1", checkScopeName)
+
+	var manifestUid uint64 = binary.BigEndian.Uint64(mcReq.Body[0:8])
+	checkManifest, err := creationEvent.GetManifestId()
+	assert.Nil(err)
+	assert.Equal(manifestUid, checkManifest)
+
+	var scopeId uint32 = binary.BigEndian.Uint32(mcReq.Body[8:12])
+	checkScopeId, err := creationEvent.GetId()
+	assert.Nil(err)
+	assert.Equal(scopeId, checkScopeId)
+
+	// Extras - first uint64 is "by_seqno" - the sequence number of this event
+	var bySeqno uint64 = binary.BigEndian.Uint64(mcReq.Extras[:8])
+	assert.Equal(bySeqno, event.Seqno)
+
+	fmt.Println("============== Test case TestCreateScopeEvent =================")
+}
+
+func TestCreateCollectionEvent(t *testing.T) {
+	assert := assert.New(t)
+	fmt.Println("============== Test case TestCreateCollectionEvent =================")
+
+	creationFile := "./unitTestData/collectionCreation.json"
+	mcReq := retrieveMcRequest(creationFile)
+
+	assert.NotNil(mcReq)
+
+	assert.Equal(uint16(511), vbOpaque(mcReq.Opaque))
+
+	tempStream := &UprStream{Vbucket: mcReq.VBucket}
+	tempBytes := 1024
+
+	event := makeUprEvent(*mcReq, tempStream, tempBytes)
+	assert.Equal(gomemcached.DCP_SYSTEM_EVENT, event.Opcode)
+	assert.True(event.IsCollectionType())
+	// The key should be the name of the scope being created
+	assert.Equal([]byte("c1"), event.Key)
+	assert.Equal(event.SystemEvent, CollectionCreate)
+
+	creationEvent := CollectionCreateEvent(event)
+	checkName, err := creationEvent.GetName()
+	assert.Nil(err)
+	assert.Equal("c1", checkName)
+
+	var manifestUid uint64 = binary.BigEndian.Uint64(mcReq.Body[0:8])
+	checkManifest, err := creationEvent.GetManifestId()
+	assert.Nil(err)
+	assert.Equal(manifestUid, checkManifest)
+
+	var colId uint32 = binary.BigEndian.Uint32(mcReq.Body[8:12])
+	checkId, err := creationEvent.GetId()
+	assert.Nil(err)
+	assert.Equal(colId, checkId)
+
+	// Extras - first uint64 is "by_seqno" - the sequence number of this event
+	var bySeqno uint64 = binary.BigEndian.Uint64(mcReq.Extras[:8])
+	assert.Equal(bySeqno, event.Seqno)
+
+	fmt.Println("============== Test case TestCreateCollectionEvent =================")
+}
+
+func TestDeleteCollectionEvent(t *testing.T) {
+	assert := assert.New(t)
+	fmt.Println("============== Test case TestDeleteCollectionEvent =================")
+
+	deletionFile := "./unitTestData/collectionDel.json"
+	mcReq := retrieveMcRequest(deletionFile)
+
+	assert.NotNil(mcReq)
+
+	// Collection drop does not send key, so vbno is not going to be the same
+	tempStream := &UprStream{Vbucket: mcReq.VBucket}
+	tempBytes := 1024
+
+	event := makeUprEvent(*mcReq, tempStream, tempBytes)
+	assert.Equal(gomemcached.DCP_SYSTEM_EVENT, event.Opcode)
+	assert.True(event.IsCollectionType())
+	assert.Equal([]byte(""), event.Key)
+	assert.Equal(event.SystemEvent, CollectionDrop)
+
+	delEvent := CollectionDropEvent(event)
+
+	var manifestUid uint64 = binary.BigEndian.Uint64(mcReq.Body[0:8])
+	checkManifest, err := delEvent.GetManifestId()
+	assert.Nil(err)
+	assert.Equal(manifestUid, checkManifest)
+
+	var colId uint32 = binary.BigEndian.Uint32(mcReq.Body[8:12])
+	checkId, err := delEvent.GetId()
+	assert.Nil(err)
+	assert.Equal(colId, checkId)
+
+	// Extras - first uint64 is "by_seqno" - the sequence number of this event
+	var bySeqno uint64 = binary.BigEndian.Uint64(mcReq.Extras[:8])
+	assert.Equal(bySeqno, event.Seqno)
+
+	fmt.Println("============== Test case TestDeleteCollectionEvent =================")
+}
+
+func TestDeleteScopeEvent(t *testing.T) {
+	assert := assert.New(t)
+	fmt.Println("============== Test case TestDeleteScopeEvent =================")
+
+	deletionFile := "./unitTestData/scopeDel.json"
+	mcReq := retrieveMcRequest(deletionFile)
+
+	assert.NotNil(mcReq)
+
+	// Collection drop does not send key, so vbno is not going to be the same
+	tempStream := &UprStream{Vbucket: mcReq.VBucket}
+	tempBytes := 1024
+
+	event := makeUprEvent(*mcReq, tempStream, tempBytes)
+	assert.Equal(gomemcached.DCP_SYSTEM_EVENT, event.Opcode)
+	assert.True(event.IsCollectionType())
+	assert.Equal([]byte(""), event.Key)
+	assert.Equal(event.SystemEvent, ScopeDrop)
+
+	delEvent := ScopeDropEvent(event)
+
+	var manifestUid uint64 = binary.BigEndian.Uint64(mcReq.Body[0:8])
+	checkManifest, err := delEvent.GetManifestId()
+	assert.Nil(err)
+	assert.Equal(manifestUid, checkManifest)
+
+	var colId uint32 = binary.BigEndian.Uint32(mcReq.Body[8:12])
+	checkId, err := delEvent.GetId()
+	assert.Nil(err)
+	assert.Equal(colId, checkId)
+
+	// Extras - first uint64 is "by_seqno" - the sequence number of this event
+	var bySeqno uint64 = binary.BigEndian.Uint64(mcReq.Extras[:8])
+	assert.Equal(bySeqno, event.Seqno)
+
+	fmt.Println("============== Test case TestDeleteScopeEvent =================")
 }
