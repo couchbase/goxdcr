05e051484a60d586c7632966fa214698cec0ba52
Lilei Chen <lilei.chen@couchbase.com>
2020-12-01 14:46:07 -0800

    MB-37283: Ensure DCP connection name is under 200 bytes
    memcached imposes a limit of 200 bytes for DCP connection name.
    XDCR generates the name like:
    xdcr:dcp_65713400edc789b05d26283428a2af88/B1/B2_127.0.0.1:12000_0:2OvLVhrDQddiFjP4shItBQ==
    
    If bucket name is long, it can exceed the limit. The name composes of:
    xdcr:<dcpId>:<25 byte randName>
    We will trim the end of source bucket name in dcpId part to ensure the name is under limit
    
    Once trimmed, the DCP connection name will look like:
    xdcr:dcp_3fe59f1b6b86d7843f0233c2166b51c5/a1234567890123/a123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789_127.0.0.1:12000_1:banE44KWp0tygnbgqEbL1g==
    
    Change-Id: I1768ec948600c9b96f9e017c9ae4035791a0c058
    Reviewed-on: http://review.couchbase.org/c/goxdcr/+/141276
    Tested-by: Lilei Chen <lilei.chen@couchbase.com>
    Reviewed-by: Lilei Chen <lilei.chen@couchbase.com>

--- base/constant.go
+++ base/constant.go
@@ -635,6 +635,9 @@ var NetTCP = "tcp"
 
 var CurrentTime = "CurrentTime"
 
+// Limit imposed by memcached
+const MaxDcpConnectionNameLength = 200
+
 /**
  * Log Redaction section
  * Spectrum 1 - User Data redaction (ud) tags
