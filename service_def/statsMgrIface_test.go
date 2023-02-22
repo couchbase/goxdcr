90cf3967f3d48dd480afdcfc1600419dfbd74e43
Neil Huang <neil.huang@couchbase.com>
2023-02-22 13:41:28 -0800

    MB-55403: enhance and reformat stats table
    Change-Id: Ic3b7185b24f783730ea9ad4a142c8767d2b6cc1e
    Reviewed-on: https://review.couchbase.org/c/goxdcr/+/187252
    Well-Formed: Restriction Checker
    Reviewed-by: Lilei Chen <lilei.chen@couchbase.com>
    Reviewed-by: Neil Huang <neil.huang@couchbase.com>
    Tested-by: Neil Huang <neil.huang@couchbase.com>

---
+++ service_def/statsMgrIface_test.go
@@ -0,0 +1,147 @@
+package service_def
+
+import (
+	"encoding/json"
+	"github.com/couchbase/goxdcr/base"
+	"github.com/stretchr/testify/assert"
+	"reflect"
+	"testing"
+)
+
+var unitTestChangesLeft = GlobalStatsTable[CHANGES_LEFT_METRIC]
+
+func TestStatsProperty_toMetaObj(t *testing.T) {
+	type fields struct {
+		MetricType        StatsUnit
+		Cardinality       MetricCardinality
+		VersionAdded      base.ServerVersion
+		VersionDeprecated base.ServerVersion
+		Description       string
+		Notes             string
+		Stability         StatsStability
+		UiName            string
+		Labels            []StatsLabel
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   *statsPropertyMetaObj
+	}{
+		{
+			name: "[POSITIVE] changes_left",
+			fields: fields{
+				MetricType:        unitTestChangesLeft.MetricType,
+				Cardinality:       unitTestChangesLeft.Cardinality,
+				VersionAdded:      unitTestChangesLeft.VersionAdded,
+				VersionDeprecated: unitTestChangesLeft.VersionDeprecated,
+				Description:       unitTestChangesLeft.Description,
+				Notes:             unitTestChangesLeft.Notes,
+				Stability:         unitTestChangesLeft.Stability,
+				UiName:            unitTestChangesLeft.UiName,
+				Labels:            unitTestChangesLeft.Labels,
+			},
+			want: &statsPropertyMetaObj{
+				Type:       unitTestChangesLeft.MetricType.Metric.String(),
+				Help:       unitTestChangesLeft.Description,
+				Added:      unitTestChangesLeft.VersionAdded.String(),
+				UiName:     unitTestChangesLeft.UiName,
+				Unit:       unitTestChangesLeft.MetricType.Unit.String(),
+				Labels:     unitTestChangesLeft.Labels.ToGenerics(),
+				Deprecated: unitTestChangesLeft.VersionDeprecated.String(),
+				Notes:      unitTestChangesLeft.Notes,
+				Stability:  unitTestChangesLeft.Stability.String(),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &StatsProperty{
+				MetricType:        tt.fields.MetricType,
+				Cardinality:       tt.fields.Cardinality,
+				VersionAdded:      tt.fields.VersionAdded,
+				VersionDeprecated: tt.fields.VersionDeprecated,
+				Description:       tt.fields.Description,
+				Notes:             tt.fields.Notes,
+				Stability:         tt.fields.Stability,
+				UiName:            tt.fields.UiName,
+				Labels:            tt.fields.Labels,
+			}
+			if got := s.toMetaObj(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("toMetaObj() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+var unitTestChangesLeftMetaObj = unitTestChangesLeft.toMetaObj()
+
+func Test_statsPropertyMetaObj_ToStatsProperty(t *testing.T) {
+	type fields struct {
+		Type       string
+		Help       string
+		Added      string
+		UiName     string
+		Unit       string
+		Labels     []interface{}
+		Deprecated string
+		Notes      string
+		Stability  string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    *StatsProperty
+		wantErr bool
+	}{
+		{
+			name: "changes_left from metaObj",
+			fields: fields{
+				Type:       unitTestChangesLeftMetaObj.Type,
+				Help:       unitTestChangesLeftMetaObj.Help,
+				Added:      unitTestChangesLeftMetaObj.Added,
+				UiName:     unitTestChangesLeftMetaObj.UiName,
+				Unit:       unitTestChangesLeftMetaObj.Unit,
+				Labels:     unitTestChangesLeftMetaObj.Labels,
+				Deprecated: unitTestChangesLeftMetaObj.Deprecated,
+				Notes:      unitTestChangesLeftMetaObj.Notes,
+				Stability:  unitTestChangesLeftMetaObj.Stability,
+			},
+			want:    &unitTestChangesLeft,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &statsPropertyMetaObj{
+				Type:       tt.fields.Type,
+				Help:       tt.fields.Help,
+				Added:      tt.fields.Added,
+				UiName:     tt.fields.UiName,
+				Unit:       tt.fields.Unit,
+				Labels:     tt.fields.Labels,
+				Deprecated: tt.fields.Deprecated,
+				Notes:      tt.fields.Notes,
+				Stability:  tt.fields.Stability,
+			}
+			got, err := s.ToStatsProperty()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ToStatsProperty() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			//if !reflect.DeepEqual(got, tt.want) {
+			if !got.SameAs(*tt.want) {
+				t.Errorf("ToStatsProperty() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+// This is just to ensure marshalling works and no panics ensue
+func Test_GlobalStatsMarshaller(t *testing.T) {
+	assert := assert.New(t)
+
+	out, err := json.Marshal(GlobalStatsTable)
+	assert.Nil(err)
+	assert.NotNil(out)
+	//fmt.Printf("%s\n", out)
+}
