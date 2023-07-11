4d13734ba60b1f52fbba9f9a37fec81f7efdaea5
Neil Huang <neil.huang@couchbase.com>
2023-07-10 17:26:29 -0700

    MB-57787: race conditions in metakv callback and settings upgrade
    Fix 1:
    Since inception metakv_change_listener has had a race condition
    that revisions could be processed out of order
    
    Fix 2:
    This can be a problem exacerbated by out-of-order writes
    of replication spec, which is caused by the upgrade path
    that is called incorrectly as part of the user issued
    doChangeReplication. The upgrade path should only be called
    during rolling upgrade / goxdcr starts, which means that
    there is only going to be one writer instead of multiple
    writers as part of the metakv callback
    
    Change-Id: I8ac677dd484e61085950ed26dbb43a30f43115d2
    Reviewed-on: https://review.couchbase.org/c/goxdcr/+/193611
    Well-Formed: Restriction Checker
    Tested-by: Neil Huang <neil.huang@couchbase.com>
    Reviewed-by: Neil Huang <neil.huang@couchbase.com>

--- metadata/replication_settings.go
+++ metadata/replication_settings.go
@@ -209,11 +209,11 @@ var ReplicateCkptIntervalConfig = &SettingsConfig{int(base.ReplicateCkptInterval
 
 var CkptSvcCacheEnabledConfig = &SettingsConfig{true, nil}
 
-var FilterBinaryDocsConfig = &SettingsConfig{false, nil}
-
 // In seconds
 var XDCRDevBucketTopologyLevacyDelayConfig = &SettingsConfig{0, &Range{0, 600}}
 
+// Note that any keys that are in the MultiValueMap should not belong here
+// Read How MultiValueMap is parsed in code for more details
 var ReplicationSettingsConfigMap = map[string]*SettingsConfig{
 	DevMainPipelineSendDelay:          XDCRDevMainPipelineSendDelayConfig,
 	DevBackfillPipelineSendDelay:      XDCRDevBackfillPipelineSendDelayConfig,
@@ -257,7 +257,6 @@ var ReplicationSettingsConfigMap = map[string]*SettingsConfig{
 	PreReplicateVBMasterCheckKey:      PreReplicateVBMasterCheckConfig,
 	ReplicateCkptIntervalKey:          ReplicateCkptIntervalConfig,
 	CkptSvcCacheEnabledKey:            CkptSvcCacheEnabledConfig,
-	FilterBinaryDocsKey:               FilterBinaryDocsConfig,
 }
 
 // Adding values in this struct is deprecated - use ReplicationSettings.Settings.Values instead
