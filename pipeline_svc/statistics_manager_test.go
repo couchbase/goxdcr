272f2b17b5547798eebad45c71c1178e939994ca
Sumukh Bhat <sumukh.bhat@couchbase.com>
2024-01-24 12:27:30 +0530

    MB-60448: Use the node's kvVbMap to calculate docs_processed when the process restarts and we have a paused replication
    - Use the node's kvVbMap instead of cluster kvVbMap to avoid negative changes_left in this scenario.
    - Also fixes the case where backfill spec exists and we wrongly set overview_stats of main pipeline as overview_stats of backfill pipeline, in this scenario.
    - After this fix, for a paused replication:
    	- Main pipeline stats will be cumulative of both main and backfill pipeline stats (following the legacy code in updateStatsForReplication)
    	- Backfill pipeline stats will be just for backfill pipeline
    
    Reviewed-on: https://review.couchbase.org/c/goxdcr/+/204331
    Reviewed-by: Neil Huang <neil.huang@couchbase.com>
    Tested-by: Sumukh Bhat <sumukh.bhat@couchbase.com>
    Change-Id: Ib875ff7b7d9cc0e57e1879f2538dbdeba88ebc9f
    Reviewed-on: https://review.couchbase.org/c/goxdcr/+/204799
    Well-Formed: Restriction Checker

--- pipeline_svc/statistics_manager_test.go
+++ pipeline_svc/statistics_manager_test.go
@@ -12,10 +12,15 @@ package pipeline_svc
 
 import (
 	"encoding/json"
+	"expvar"
 	"fmt"
-	"github.com/couchbase/goxdcr/base"
 	"io/ioutil"
+	"strconv"
 	"testing"
+
+	"github.com/couchbase/goxdcr/base"
+	"github.com/couchbase/goxdcr/metadata"
+	"github.com/couchbase/goxdcr/pipeline"
 
 	"github.com/couchbase/goxdcr/base/filter"
 	"github.com/couchbase/goxdcr/utils"
@@ -24,8 +29,9 @@ import (
 	commonReal "github.com/couchbase/goxdcr/common"
 	common "github.com/couchbase/goxdcr/common/mocks"
 	"github.com/couchbase/goxdcr/log"
-	"github.com/couchbase/goxdcr/metadata"
 	"github.com/couchbase/goxdcr/parts"
+
+	pm "github.com/couchbase/goxdcr/pipeline/mocks"
 	service_def2 "github.com/couchbase/goxdcr/service_def"
 	service_def "github.com/couchbase/goxdcr/service_def/mocks"
 	utilities "github.com/couchbase/goxdcr/utils/mocks"
@@ -34,11 +40,33 @@ import (
 	mock "github.com/stretchr/testify/mock"
 )
 
+var OverviewStatsBackfill, OverviewStatsMain *expvar.Map
+
 var pipelineTopic string = "topic"
 
 var testDCPPart string = "testDCP"
 var testXmemPart string = "testXmem"
 var testRouter string = "testRouter"
+
+var seqnoProcessed int64 = 2500
+var seqnoTotal int64 = 3000
+var backfillEndSeqno int64 = 2800
+var backfillStartSeqno int64 = 0
+var numVbs int64 = 1024
+
+// clusterKVVbMap = {myself: [0-499], peer1: [500-699], peer2: [700-1023]}
+var startVb int64 = 0
+var endVbForMyself int64 = 500
+var endVbForPeer1 int64 = 700
+var endVb int64 = numVbs
+var myVbNos int64 = endVbForMyself - startVb
+
+var expectedBackfillTotalChanges int64 = (backfillEndSeqno - backfillStartSeqno) * myVbNos           // 2800*500 = 1400000
+var expectedBackfillDocsProcessed int64 = seqnoProcessed * myVbNos                                   // 2500*500 = 1250000
+var expectedBackfillChangesLeft int64 = expectedBackfillTotalChanges - expectedBackfillDocsProcessed // 150000
+var expectedDocsProcessed int64 = seqnoProcessed*myVbNos + expectedBackfillDocsProcessed             // 2500*500 + 1250000 = 2500000
+var expectedTotalChanges int64 = seqnoTotal*myVbNos + expectedBackfillTotalChanges                   // 3000*500 + 1400000 = 2900000
+var expectedChangesLeft int64 = expectedTotalChanges - expectedDocsProcessed                         // 400000
 
 func RetrieveUprFile(fileName string) (*mcc.UprEvent, error) {
 	data, err := ioutil.ReadFile(fileName)
@@ -445,4 +473,243 @@ func TestStatsMgrWithFilteringStats(t *testing.T) {
 	assert.Equal(int64(5), (routerCollector.component_map[testRouter][service_def2.DOCS_FILTERED_METRIC]).(metrics.Counter).Count())
 
 	fmt.Println("============== Test case end: TestStatsMgrWithFilteringStats =================")
+}
+
+func getDocsProcessedFromOverviewStats(overviewStats *expvar.Map) int64 {
+	docs_processed, err := strconv.ParseInt(overviewStats.Get(service_def2.DOCS_PROCESSED_METRIC).String(), base.ParseIntBase, base.ParseIntBitSize)
+	if err != nil {
+		panic("FIXME, invalid metrics type for docs_processed")
+	}
+	return docs_processed
+}
+
+func getChangesLeftFromOverviewStats(overviewStats *expvar.Map) int64 {
+	changes_left, err := strconv.ParseInt(overviewStats.Get(service_def2.CHANGES_LEFT_METRIC).String(), base.ParseIntBase, base.ParseIntBitSize)
+	if err != nil {
+		panic("FIXME, invalid metrics type for changes_left")
+	}
+	return changes_left
+}
+
+func setupPausedStatsMocks(backfillSvc *service_def.BackfillReplSvc, remoteSvc *service_def.RemoteClusterSvc, ckptSvc *service_def.CheckpointsService, ref *metadata.RemoteClusterReference, targetKVVbMap map[string][]uint16,
+	replSpec *metadata.ReplicationSpecification, assert *assert.Assertions, mainIsNil, backfillIsNil bool) (*service_def.BucketTopologySvc, func() map[string]pipeline.ReplicationStatusIface) {
+	cap := metadata.UnitTestGetCollectionsCapability()
+	remoteSvc.On("GetCapability", mock.Anything).Return(cap, nil)
+	remoteSvc.On("RemoteClusterByUuid", mock.Anything, mock.Anything).Return(ref, nil)
+
+	clusterMap := make(base.KvVBMapType)
+	clusterMap["myself"] = make([]uint16, 0)
+	clusterMap["peer1"] = make([]uint16, 0)
+	clusterMap["peer2"] = make([]uint16, 0)
+	targetKVVbMap["myself"] = make([]uint16, 0)
+	for i := startVb; i < endVbForMyself; i++ {
+		targetKVVbMap["myself"] = append(targetKVVbMap["myself"], uint16(i))
+		clusterMap["myself"] = append(clusterMap["myself"], uint16(i))
+	}
+
+	for i := endVbForMyself; i < endVbForPeer1; i++ {
+		clusterMap["peer1"] = append(clusterMap["peer1"], uint16(i))
+	}
+
+	for i := endVbForPeer1; i < endVb; i++ {
+		clusterMap["peer2"] = append(clusterMap["peer2"], uint16(i))
+	}
+
+	var highMap base.HighSeqnosMapType
+	highMap = make(base.HighSeqnosMapType)
+	mp := make(map[uint16]uint64)
+	highMap["myself"] = &mp
+	vbList := make([]uint16, 0)
+	vbTasks := metadata.NewVBTasksMap()
+	ts := &metadata.BackfillVBTimestamps{
+		StartingTimestamp: &base.VBTimestamp{Seqno: uint64(backfillStartSeqno)},
+		EndingTimestamp:   &base.VBTimestamp{Seqno: uint64(backfillEndSeqno)},
+	}
+	for i := 0; i < 500; i++ {
+		mp[uint16(i)] = uint64(seqnoTotal)
+		vbList = append(vbList, uint16(i))
+		backfillTasks := metadata.NewBackfillTasks()
+		backfillTasks.List = append(backfillTasks.List, metadata.NewBackfillTask(ts, []metadata.CollectionNamespaceMapping{}))
+		vbTasks.VBTasksMap[uint16(i)] = &backfillTasks
+	}
+
+	ckpts := make(map[uint16]*metadata.CheckpointsDoc, numVbs)
+	ckptRecordsList := []*metadata.CheckpointRecord{
+		{
+			Seqno: uint64(seqnoProcessed),
+		}}
+	var ckptlist metadata.CheckpointRecordsList = ckptRecordsList
+	for i := 0; i < int(numVbs); i++ {
+		ckpts[uint16(i)] = &metadata.CheckpointsDoc{
+			Checkpoint_records: ckptlist,
+		}
+	}
+
+	ckptSvc.On("CheckpointsDocs", mock.Anything, mock.Anything).Return(ckpts, nil)
+	var ckpt *metadata.CheckpointsDoc
+	ckptSvc.On("CheckpointsDoc", mock.Anything, mock.Anything).Run(func(args mock.Arguments) {
+		vbno, ok := args.Get(1).(uint16)
+		if !ok {
+			panic("FIXME, vbno not uint16")
+		}
+		ckpt = ckpts[vbno]
+	}).Return(ckpt, nil)
+
+	notx := &service_def.SourceNotification{}
+	notx.On("GetHighSeqnosMap").Return(highMap)
+	notx.On("GetSourceVBMapRO").Return(base.KvVBMapType(targetKVVbMap))
+	notx.On("GetKvVbMapRO").Return(clusterMap)
+	notx.On("Recycle").Return()
+	bucketSvc := &service_def.BucketTopologySvc{}
+	ch := make(chan service_def2.SourceNotification, 1)
+	bucketSvc.On("SubscribeToLocalBucketHighSeqnosFeed", mock.Anything, mock.Anything, mock.Anything).Run(func(mock.Arguments) {
+		ch <- notx
+	}).Return(ch, nil, nil)
+	bucketSvc.On("SubscribeToLocalBucketFeed", mock.Anything, mock.Anything, mock.Anything).Run(func(args mock.Arguments) {
+		ch <- notx
+	}).Return(ch, nil, nil)
+	bucketSvc.On("UnSubscribeToLocalBucketHighSeqnosFeed", mock.Anything, mock.Anything).Return(nil)
+	bucketSvc.On("UnSubscribeLocalBucketFeed", mock.Anything, mock.Anything).Return(nil)
+
+	backSpec := &metadata.BackfillReplicationSpec{Id: pipelineTopic, VBTasksMap: vbTasks}
+	backfillSvc.On("BackfillReplSpec", mock.Anything, mock.Anything).Return(backSpec, nil)
+
+	repl_status := &pm.ReplicationStatusIface{}
+	OverviewStatsMain = new(expvar.Map).Init()
+	OverviewStatsMain.Add(service_def2.DOCS_PROCESSED_METRIC, 90)
+	OverviewStatsMain.Add(service_def2.CHANGES_LEFT_METRIC, 90)
+	for _, statsToInitialize := range StatsToInitializeForPausedReplications {
+		OverviewStatsMain.Add(statsToInitialize, 0)
+	}
+	repl_status.On("SetVbList", mock.Anything)
+	repl_status.On("Spec").Return(replSpec)
+	repl_status.On("RuntimeStatus", mock.Anything).Return(pipeline.Paused)
+	repl_status.On("VbList").Return([]uint16{0})
+	repl_status.On("RepId").Return(pipelineTopic)
+	repl_status.On("Errors").Return(pipeline.PipelineErrorArray{})
+
+	backfillPipMatcher := mock.MatchedBy(func(param interface{}) bool {
+		type_, ok := param.(commonReal.PipelineType)
+		if !ok {
+			fmt.Printf("NOT PIPELINE TYPE")
+			return false
+		}
+		return type_ == commonReal.BackfillPipeline
+	})
+	mainPipMatcher := mock.MatchedBy(func(param interface{}) bool {
+		type_, ok := param.(commonReal.PipelineType)
+		if !ok {
+			fmt.Printf("NOT PIPELINE TYPE")
+			return false
+		}
+		return type_ == commonReal.MainPipeline
+	})
+
+	if mainIsNil {
+		OverviewStatsMain = nil
+	} else {
+		OverviewStatsMain = new(expvar.Map).Init()
+		OverviewStatsMain.Add(service_def2.DOCS_PROCESSED_METRIC, 100)
+		OverviewStatsMain.Add(service_def2.CHANGES_LEFT_METRIC, 100)
+		for _, statsToInitialize := range StatsToInitializeForPausedReplications {
+			OverviewStatsMain.Add(statsToInitialize, 0)
+		}
+	}
+
+	if backfillIsNil {
+		OverviewStatsBackfill = nil
+	} else {
+		OverviewStatsBackfill = new(expvar.Map).Init()
+		OverviewStatsBackfill.Add(service_def2.DOCS_PROCESSED_METRIC, 100)
+		OverviewStatsBackfill.Add(service_def2.CHANGES_LEFT_METRIC, 100)
+		for _, statsToInitialize := range StatsToInitializeForPausedReplications {
+			OverviewStatsBackfill.Add(statsToInitialize, 0)
+		}
+	}
+
+	repl_status.On("SetOverviewStats", mock.Anything, backfillPipMatcher).Run(func(args mock.Arguments) {
+		os, ok := args.Get(0).(*expvar.Map)
+		if !ok {
+			panic("FIXME, SetOverviewStats of backfill")
+		}
+		OverviewStatsBackfill = os
+	})
+
+	repl_status.On("SetOverviewStats", mock.Anything, mainPipMatcher).Run(func(args mock.Arguments) {
+		os, ok := args.Get(0).(*expvar.Map)
+		if !ok {
+			panic("FIXME, SetOverviewStats of main")
+		}
+		OverviewStatsMain = os
+	})
+
+	repl_status.On("GetOverviewStats", mainPipMatcher).Return(OverviewStatsMain)
+	repl_status.On("GetOverviewStats", backfillPipMatcher).Return(OverviewStatsBackfill)
+
+	replGetter := func() map[string]pipeline.ReplicationStatusIface {
+		return map[string]pipeline.ReplicationStatusIface{
+			pipelineTopic: repl_status,
+		}
+	}
+
+	return bucketSvc, replGetter
+}
+
+func assertPausedStats(OverviewStatsMain, OverviewStatsBackfill *expvar.Map, assert *assert.Assertions) {
+	cl := getChangesLeftFromOverviewStats(OverviewStatsMain)
+	dp := getDocsProcessedFromOverviewStats(OverviewStatsMain)
+	assert.Equal(expectedDocsProcessed, dp)
+	assert.Equal(expectedChangesLeft, cl)
+	bcl := getChangesLeftFromOverviewStats(OverviewStatsBackfill)
+	bdp := getDocsProcessedFromOverviewStats(OverviewStatsBackfill)
+	assert.Equal(expectedBackfillDocsProcessed, bdp)
+	assert.Equal(expectedBackfillChangesLeft, bcl)
+}
+
+func TestPausedReplicationStats1a(t *testing.T) {
+	fmt.Println("============== Test case start: TestPausedReplicationContructStats 1a =================")
+	defer fmt.Println("============== Test case start: TestPausedReplicationContructStats 1a =================")
+
+	assert := assert.New(t)
+	logger, _, _, _, _, _, replSpec, _, ckptSvc, _, remoteSvc, _, targetKVVbMap, ref, _, _, _, _, backfillSvc, _ := setupBoilerPlate()
+	// main is nil, backfill is not
+	bucketSvc, replGetter := setupPausedStatsMocks(backfillSvc, remoteSvc, ckptSvc, ref, targetKVVbMap, replSpec, assert, true, false)
+	UpdateStats(ckptSvc, logger, remoteSvc, backfillSvc, bucketSvc, replGetter)
+	assertPausedStats(OverviewStatsMain, OverviewStatsBackfill, assert)
+}
+
+func TestPausedReplicationStats1b(t *testing.T) {
+	fmt.Println("============== Test case start: TestPausedReplicationContructStats 1b =================")
+	defer fmt.Println("============== Test case start: TestPausedReplicationContructStats 1b =================")
+
+	assert := assert.New(t)
+	logger, _, _, _, _, _, replSpec, _, ckptSvc, _, remoteSvc, _, targetKVVbMap, ref, _, _, _, _, backfillSvc, _ := setupBoilerPlate()
+	// main is nil, backfill is nil
+	bucketSvc, replGetter := setupPausedStatsMocks(backfillSvc, remoteSvc, ckptSvc, ref, targetKVVbMap, replSpec, assert, true, true)
+	UpdateStats(ckptSvc, logger, remoteSvc, backfillSvc, bucketSvc, replGetter)
+	assertPausedStats(OverviewStatsMain, OverviewStatsBackfill, assert)
+}
+
+func TestPausedReplicationStats2a(t *testing.T) {
+	fmt.Println("============== Test case start: TestPausedReplicationUpdateStats 2a =================")
+	defer fmt.Println("============== Test case start: TestPausedReplicationUpdateStats 2a =================")
+
+	assert := assert.New(t)
+	logger, _, _, _, _, _, replSpec, _, ckptSvc, _, remoteSvc, _, targetKVVbMap, ref, _, _, _, _, backfillSvc, _ := setupBoilerPlate()
+	// main is not nil, backfill is not nil
+	bucketSvc, replGetter := setupPausedStatsMocks(backfillSvc, remoteSvc, ckptSvc, ref, targetKVVbMap, replSpec, assert, false, false)
+	UpdateStats(ckptSvc, logger, remoteSvc, backfillSvc, bucketSvc, replGetter)
+	assertPausedStats(OverviewStatsMain, OverviewStatsBackfill, assert)
+}
+
+func TestPausedReplicationStats2b(t *testing.T) {
+	fmt.Println("============== Test case start: TestPausedReplicationUpdateStats 2b =================")
+	defer fmt.Println("============== Test case start: TestPausedReplicationUpdateStats 2b =================")
+
+	assert := assert.New(t)
+	logger, _, _, _, _, _, replSpec, _, ckptSvc, _, remoteSvc, _, targetKVVbMap, ref, _, _, _, _, backfillSvc, _ := setupBoilerPlate()
+	// main is not nil, backfill is nil
+	bucketSvc, replGetter := setupPausedStatsMocks(backfillSvc, remoteSvc, ckptSvc, ref, targetKVVbMap, replSpec, assert, false, true)
+	UpdateStats(ckptSvc, logger, remoteSvc, backfillSvc, bucketSvc, replGetter)
+	assertPausedStats(OverviewStatsMain, OverviewStatsBackfill, assert)
 }
