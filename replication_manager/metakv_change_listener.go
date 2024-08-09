0daba88f3613cb699f2ee100906f5f3eb2cbecf1
darshan-na <darshan.na@couchbase.com>
2024-08-09 15:34:50 +0530

    MB-63013: Added support for handling type mismatch errors
    Change-Id: Id3054f877ebdd67685472cd5f1a7e3df5a425696
    Reviewed-on: https://review.couchbase.org/c/goxdcr/+/214575
    Well-Formed: Restriction Checker
    Reviewed-by: Darshan N A <darshan.na@couchbase.com>
    Tested-by: Darshan N A <darshan.na@couchbase.com>

--- replication_manager/metakv_change_listener.go
+++ replication_manager/metakv_change_listener.go
@@ -767,28 +767,41 @@ func (pscl *GlobalSettingChangeListener) globalSettingChangeHandlerCallback(sett
 		oldGoGCValue := debug.SetGCPercent(newSetting.GoGC)
 		pscl.logger.Infof("Successfully changed  GOGC setting from(old) %v to(New) %v\n", oldGoGCValue, newSetting.GoGC)
 	}
-	// represents a json containing the logLevel for individual services
-	genericServicesLogLevel := newSetting.Settings.Values[metadata.GenericServicesLogLevelKey].(string)
-	serviceToLogLevelMap, err1 := base.ValidateAndConvertStringToJsonType(genericServicesLogLevel)
-	if err1 != nil {
-		return fmt.Errorf("Failed to apply the LogLevel for specified services. err: %v\n", err1)
-	}
-	for service, logLevelStr := range serviceToLogLevelMap {
-		log.ServiceToLoggerContext.Lock.RLock()
-		context, ok := log.ServiceToLoggerContext.ServiceToContextMap[service]
-		log.ServiceToLoggerContext.Lock.RUnlock()
-		if !ok {
-			pscl.logger.Errorf("No LoggerContext exists for the service %v\n", service)
-			continue
-		}
-		loglevel, err1 := log.LogLevelFromStr(logLevelStr.(string))
-		if err1 != nil {
-			pscl.logger.Errorf("%v, setting the loglevel to Info for %v", err, service)
-			//reset it to logLevelInfo
-			loglevel = log.LogLevelInfo
-		}
-		if context.Log_level != loglevel {
-			context.SetLogLevel(loglevel)
+	if newSetting.Settings != nil {
+		// represents a json containing the logLevel for individual services
+		genericServicesLogLevel, ok := newSetting.Settings.Values[metadata.GenericServicesLogLevelKey]
+		if ok {
+			genericServicesLogLevelStr, valid := genericServicesLogLevel.(string)
+			if !valid {
+				return fmt.Errorf("failed to apply the LogLevel for specified services. err=Invalid type %T for genericServicesLogLevel. Expected string", genericServicesLogLevel)
+			}
+			serviceToLogLevelMap, err1 := base.ValidateAndConvertStringToJsonType(genericServicesLogLevelStr)
+			if err1 != nil {
+				return fmt.Errorf("failed to apply the LogLevel for specified services. err: %v\n", err1)
+			}
+			for service, logLevelIface := range serviceToLogLevelMap {
+				log.ServiceToLoggerContext.Lock.RLock()
+				context, ok := log.ServiceToLoggerContext.ServiceToContextMap[service]
+				log.ServiceToLoggerContext.Lock.RUnlock()
+				if !ok {
+					pscl.logger.Errorf("no LoggerContext exists for the service %v", service)
+					continue
+				}
+				logLevelStr, valid := logLevelIface.(string)
+				if !valid {
+					pscl.logger.Errorf("failed to apply the LogLevel for service %v. err=Invalid type %T for logLevel. Expected string", service, logLevelIface)
+					continue
+				}
+				loglevel, err1 := log.LogLevelFromStr(logLevelStr)
+				if err1 != nil {
+					pscl.logger.Errorf("%v, setting the loglevel to Info for %v", err, service)
+					//reset it to logLevelInfo
+					loglevel = log.LogLevelInfo
+				}
+				if context.Log_level != loglevel {
+					context.SetLogLevel(loglevel)
+				}
+			}
 		}
 	}
 	return nil
