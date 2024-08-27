e2a8635748b5acb47e67ac5827b3bec49c92ac72
Neil Huang <neil.huang@couchbase.com>
2024-08-27 10:33:54 -0700

    MB-52889: test suite for variable VB
    Change-Id: Ib76b9e126f6ab63141397c69db42eca461c2a58f

---
+++ tools/testScripts/runVariableVBTests.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Copyright 2019-Present Couchbase, Inc.
+#
+# Use of this software is governed by the Business Source License included in
+# the file licenses/BSL-Couchbase.txt.  As of the Change Date specified in that
+# file, in accordance with the Business Source License, use of this software
+# will be governed by the Apache License, Version 2.0, included in the file
+# licenses/APL2.txt.
+
+testCasesDirectory="variableVBTestCases"
+
+# main logic all exist elsewhere
+. ./commonTestRunner.shlib
+if (($? != 0)); then
+	exit $?
+fi
