1107a12416aac2738515511dcd66898d34cd40d9
Neil Huang <neil.huang@couchbase.com>
2024-10-28 21:11:56 -0700

    MB-52889: remove variableVB internal settings and safeguard
    Change-Id: I703aaa73a3954a8955b35d512bac5f0724a90a7c

--- metadata/internal_settings.go
+++ metadata/internal_settings.go
@@ -303,8 +303,6 @@ const (
 	CLogStatsLoggingMaxFreqKey  = "CLogStatsLoggingMaxFreq"
 
 	TempMCErrorDisplayDelayFactorKey = "TempMCErrorDisplayDelayFactor"
-
-	VariableVBSupportKey = "VariableVBSupport"
 )
 
 var TopologyChangeCheckIntervalConfig = &SettingsConfig{10, &Range{1, 100}}
@@ -445,7 +443,6 @@ var CLogSkipTlsVerifyConfig = &SettingsConfig{base.CLogSkipTlsVerify, nil}
 var CLogResourceManagerBoostConfig = &SettingsConfig{base.CLogResourceManagerBoost, &Range{0, 10000}}
 var CLogStatsLoggingMaxFreqConfig = &SettingsConfig{int(base.CLogStatsLoggingMaxFreqInterval / time.Second), &Range{30, 24 * 60 * 60 /* 1 day */}}
 var TempMCErrorDisplayDelayFactorConfig = &SettingsConfig{base.TempMCErrorDisplayDelayFactor, &Range{1, 100}}
-var VariableVBSupportConfig = &SettingsConfig{base.VariableVBSupport, nil}
 
 var XDCRInternalSettingsConfigMap = map[string]*SettingsConfig{
 	TopologyChangeCheckIntervalKey:                TopologyChangeCheckIntervalConfig,
@@ -586,7 +583,6 @@ var XDCRInternalSettingsConfigMap = map[string]*SettingsConfig{
 	CLogResourceManagerBoostKey:                   CLogResourceManagerBoostConfig,
 	CLogStatsLoggingMaxFreqKey:                    CLogStatsLoggingMaxFreqConfig,
 	TempMCErrorDisplayDelayFactorKey:              TempMCErrorDisplayDelayFactorConfig,
-	VariableVBSupportKey:                          VariableVBSupportConfig,
 }
 
 func InitConstants(xmemMaxIdleCountLowerBound int, xmemMaxIdleCountUpperBound int) {
