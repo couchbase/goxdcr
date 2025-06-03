b0e47c5d39ba1da363a65a7082aa34d06caee65d
Sumukh Bhat <sumukh.bhat@couchbase.com>
2025-06-03 18:41:11 +0530

    MB-66308: Set HELO UserAgent name for RemoteClusterAgent connections
    Change-Id: I2b78f0e23d69f1bb6c89d5abbeb7c70f81a296d3
    Reviewed-on: https://review.couchbase.org/c/goxdcr/+/228676
    Tested-by: Sumukh Bhat <sumukh.bhat@couchbase.com>
    Reviewed-by: Sumukh Bhat <sumukh.bhat@couchbase.com>
    Well-Formed: Restriction Checker

--- metadata_svc/remote_cluster_service.go
+++ metadata_svc/remote_cluster_service.go
@@ -2191,7 +2191,7 @@ func (agent *RemoteClusterAgent) getRemoteMemcachedComponent(bucketName string) 
 		return agent.reference.Clone()
 	}).SetAlternateAddressChecker(func(reference *metadata.RemoteClusterReference) (bool, error) {
 		return agent.UsesAlternateAddress()
-	})
+	}).SetUserAgent(userAgentStr)
 
 	return remoteMemcachedComponent
 }
