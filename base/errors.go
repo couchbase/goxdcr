7a3afbed40c0dbad3b8cd795132c7abcf8f1cd04
Aditya Sood <aditya.sood@couchbase.com>
2026-03-24 16:13:23 +0530

    MB-70973: Adds cluster-compat check for `VersionForCLoggerSupport` on Target cluster
    Change-Id: Ibf7b531005cf2cea9a00ae225d78360eef8c08e7
    Reviewed-on: https://review.couchbase.org/c/goxdcr/+/242260
    Reviewed-by: Sumukh Bhat <sumukh.bhat@couchbase.com>
    Tested-by: Aditya Sood <aditya.sood@couchbase.com>

--- base/errors.go
+++ base/errors.go
@@ -102,6 +102,7 @@ var (
 	ErrorHostNameEmpty                           = errors.New("Hostname is empty")
 	ErrorReplicationSpecNotActive                = errors.New("replication specification not found or no longer active")
 	ErrorCLoggingMixedModeUnsupported            = errors.New("not all nodes support conflict logging feature")
+	ErrorCLoggingTargetUnsupported               = errors.New("Target cluster does not support conflict-logging feature")
 	ErrorSubdocMaxPathLimitBreached              = fmt.Errorf("subdoc max path limit breached")
 	ErrorSeamlessCredsChangeMixedModeUnsupported = errors.New("Seamless credentials change is not supported in mixed-mode clusters. Upgrade all nodes to version >=8.1 to enable credential staging.")
 	ErrUpsertAlreadyOccurring                    = fmt.Errorf("Error upserting mapping as an operation is happening already")
